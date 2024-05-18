b8176ab7e318e782c03e689d40be5043e7dc951a
vidalgmartin <vidalgmartin@hotmail.com>
2024-05-18 14:34:16 -0400

    leaflet marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,18 @@
 import { useEffect, useRef } from 'react'
 import { Map as LeafletMap } from 'leaflet'
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import 'leaflet/dist/images/marker-icon.png'
-import 'leaflet/dist/images/marker-shadow.png'
 import './components.css'
+import 'leaflet/dist/leaflet.css'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+import L from 'leaflet'
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
 
 export default function Map({ place }) {
     const mapRef = useRef(<LeafletMap></LeafletMap>)
@@ -24,7 +32,7 @@ export default function Map({ place }) {
             className="map"
         >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {place && <Marker position={[place.latitude, place.longitude]} />}
+            {place && <Marker position={[place.latitude, place.longitude]}></Marker>}
         </MapContainer>
     )
 }
